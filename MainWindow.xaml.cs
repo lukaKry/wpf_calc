39ee9498a3812d3bb1c08ca396ed54100f10fb4e
lukaKry <kryszak.lukasz@gmail.com>
2021-05-30 10:51:39 +0200

    UI Architecture complete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,120 +22,27 @@ namespace WpfApp_Calc
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private string _Memory;
-        public string Memory
+        private Class1 _class1;
+        public Class1 Class1
         {
-            get { return _Memory; }
+            get { return _class1; }
             set
             {
-                _Memory = value;
-                PropertyChanged?.Invoke(this, new(nameof(Memory)));
+                _class1 = value;
+                PropertyChanged?.Invoke(this, new(nameof(Class1)));
             }
         }
-
-        private string _Display;
-        public string Display
-        {
-            get { return _Display; }
-            set
-            {
-                _Display = value;
-                PropertyChanged?.Invoke(this, new(nameof(Display)));
-            }
-        }
-
-        private string _AdditionalDisplay;
-        public string AdditionalDisplay
-        {
-            get { return _AdditionalDisplay; }
-            set
-            {
-                _AdditionalDisplay = value;
-                PropertyChanged?.Invoke(this, new(nameof(AdditionalDisplay)));
-            }
-        }
-        public List<string> Equation { get; set; } = new();
 
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+            _class1 = new Class1();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void OnCalculate(object sender, RoutedEventArgs e)
-        {
-            Equation.Add(Display);
-            AdditionalDisplay = "";
-            string test = string.Join("", Equation);
-
-            if (!string.IsNullOrEmpty(Display) && Equation.Count > 0)
-            {
-                if (Equation[1] == "/" && (Equation[2] == "0" || Equation[2] == "0."))
-                {
-                    //if(Equation[2] == "0" || Equation[2] == "0.") 
-                        MessageBox.Show("nieładnie dzielić przez 0");
-                }
-                else
-                {
-                    Display = new DataTable().Compute(test, null).ToString().Replace(",", ".");
-                    if (Display.Length > 15) Display = Display.Substring(0, 15);
-                }
-            }
-            Equation.RemoveAll(q => true);
-        }
-
-        private void OnNumberButtonClicked(object sender, RoutedEventArgs e)
-        {
-            Button b = (Button)sender;
-            Display += b.Uid;
-           // if (Display.EndsWith("..")) Display = Display.Substring(0, Display.Length - 1);
-           if( b.Uid == ".")
-            if (Display[0..^1].Contains(".")) 
-              Display = Display[0..^1];
-
-        }
-
-        private void OnSymbolButtonClicked(object sender, RoutedEventArgs e)
-        {
-            Button b = (Button)sender;
-            if (!string.IsNullOrEmpty(Display))
-            {
-                Equation.Add(Display);
-                AdditionalDisplay = b.Uid;
-                Equation.Add(b.Uid);
-                Display = "";
-            }
-            else
-            {
-                if (Equation.Count != 0)
-                {
-                    Equation[^1] = b.Uid;
-                    AdditionalDisplay = b.Uid;
-                }
-            }
-        }
-
-        private void OnClear(object sender, RoutedEventArgs e)
-        {
-            Display = "";
-            AdditionalDisplay = "";
-            Equation.RemoveAll(q => true);
-        }
-
-        private void OnMemory(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(Display))
-            {
-                Display = Memory;
-            }
-            else
-            {
-                Memory = Display;
-                Display = "";
-            }
-        }
+        
     }
 }
